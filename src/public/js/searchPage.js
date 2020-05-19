4dd84907a8dde687ed6aa7949f37a883e811d587
KSSBro <shashwatdeadcliff@gmail.com>
2020-05-20 02:33:26 +0530

    added jquery | added sticky topbar

--- src/public/js/searchPage.js
+++ src/public/js/searchPage.js
@@ -1,1 +1,9 @@
-console.log(query);
+$(window).on('scroll',function(){
+    if (Math.round($(window).scrollTop()) > 400) {
+        $("#topbar").removeClass('topbar-noscroll')
+        $("#topbar").addClass("topbar-scroll");
+    } else {
+        $("#topbar").removeClass('topbar-scroll');
+        $("#topbar").addClass("topbar-noscroll");
+    }
+});
